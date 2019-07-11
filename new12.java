97bdf7aa12c00e84363aacde0e279033155b309c
vishnukrishna189 <48021110+vishnukrishna189@users.noreply.github.com>
2019-07-11 17:00:20 +0530

    Create new12.java

---
+++ new12.java
@@ -0,0 +1,11 @@
+public class jenkinsexample 
+
+{
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("vishnu");
+	 System.out.println("k")
+		 }
+
+}
