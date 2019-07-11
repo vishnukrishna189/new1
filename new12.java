cf655e8f5584fb7b2b3d904bc5473f860173e9d7
vishnukrishna189 <48021110+vishnukrishna189@users.noreply.github.com>
2019-07-11 17:16:13 +0530

    Update new12.java

--- new12.java
+++ new12.java
@@ -5,7 +5,7 @@ public class jenkinsexample
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("vishnu");
-	 System.out.println("k vishnu")
+	 System.out.println("k vishnu is great")
 		 }
 
 }
